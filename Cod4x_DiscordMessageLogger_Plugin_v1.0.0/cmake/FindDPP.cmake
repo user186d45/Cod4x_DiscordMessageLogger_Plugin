9c7ffbcb343f94ee6c0c5095138743646907ba01
user186d45 <dowhcosv@protonmail.com>
2025-03-24 14:06:25 +0330

    Delete Cod4x_DiscordMessageLogger_Plugin_v1.0.0 directory

--- Cod4x_DiscordMessageLogger_Plugin_v1.0.0/cmake/FindDPP.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Searching for DPP in: ${DPP_ROOT_DIR}/lib /usr/local/lib")
-
-find_path(DPP_INCLUDE_DIR NAMES dpp/dpp.h HINTS ${DPP_ROOT_DIR} /usr/local/include)
-
-find_library(DPP_LIBRARIES NAMES dpp "libdpp.so" HINTS ${DPP_ROOT_DIR}/lib /usr/local/lib)
-	 
-include(FindPackageHandleStandardArgs)
-	 
-find_package_handle_standard_args(DPP DEFAULT_MSG DPP_LIBRARIES DPP_INCLUDE_DIR)
